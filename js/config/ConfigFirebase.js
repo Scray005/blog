c503ac3453beaf365a1215dac1a42faca6a03352
andres <and.005@hotmail.com>
2017-05-14 20:38:25 -0500

    seminario

---
+++ js/config/ConfigFirebase.js
@@ -0,0 +1,8 @@
+var varConfig = {
+    apiKey: "AIzaSyDxg_UKdFct-N_Llqovkps7rpYr7aGcJQ8",
+    authDomain: "blog-ucc.firebaseapp.com",
+    databaseURL: "https://blog-ucc.firebaseio.com",
+    projectId: "blog-ucc",
+    storageBucket: "blog-ucc.appspot.com",
+    messagingSenderId: "738807770067"
+};
